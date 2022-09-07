560c1fe5a387b2ca34b3c62d438ef66f974af186
Chorchh <jorcapai@gmail.com>
2022-09-07 19:40:51 -0300

    hicimos EJ3

--- main.js
+++ main.js
@@ -74,77 +74,126 @@ let peliculas = [
 
 
 
-//   EJ 1
-const lista = document.getElementById("lista");
-const input = document.getElementById("toggle-menu");
+// //   EJ 1
+// const lista = document.getElementById("lista");
+// const input = document.getElementById("toggle-menu");
 
-input.addEventListener("click", () => {
-    lista.classList.toggle("mostrar");
-});
+// input.addEventListener("click", () => {
+//     lista.classList.toggle("mostrar");
+// });
 
 
-// EJ 2
-const form = document.getElementById("form");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const ingresar = document.querySelector(".ingresar");
-const msgEmail = document.querySelector(".msgEmail");
-const msgPass = document.querySelector(".msgPass");
+// // EJ 2
+// const form = document.getElementById("form");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const ingresar = document.querySelector(".ingresar");
+// const msgEmail = document.querySelector(".msgEmail");
+// const msgPass = document.querySelector(".msgPass");
 
-// validar email y pass
-const emailValid = e => {
-  e.preventDefault();
+// // validar email y pass
+// const emailValid = e => {
+//   e.preventDefault();
 
-  const emailInput = email.value.trim();
+//   const emailInput = email.value.trim();
 
-  if(!emailInput.length){
-    alert("El campo email esta vacio")
-    return;
-  } else if (/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,4})+$/.test(emailInput)){
-    msgEmail.innerHTML = "email valido"
-    email.style.border = "3px solid green"
-  } else {
-    msgEmail.innerHTML = "email incorrecto"
-    email.style.border = "3px solid red"
-  };
+//   if(!emailInput.length){
+//     alert("El campo email esta vacio")
+//     return;
+//   } else if (/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,4})+$/.test(emailInput)){
+//     msgEmail.innerHTML = "email valido"
+//     email.style.border = "3px solid green"
+//   } else {
+//     msgEmail.innerHTML = "email incorrecto"
+//     email.style.border = "3px solid red"
+//   };
+// };
+
+
+// const passValid = e => {
+//   e.preventDefault();
+
+//   const passInput = password.value.trim();
+//   const lowerCase = /[a-z]/g;
+//   const upperCase = /[A-Z]/g;
+//   const number = /[0-9]/g;
+//   const symbol = /(?=.[!@#$%^&\-*])/g;
+
+//   if(passInput == "") {
+//     alert("El campo contraseña esta vacio");
+//     return;
+//   }
+
+
+//   if(!passInput.match(lowerCase)) {
+//     msgPass.textContent = "Debes incluir una minuscula";
+//   } else if (!passInput.match(upperCase)) {
+//     msgPass.textContent = "Debes incluir una mayuscula"
+//   } else if (!passInput.match(number)) {
+//     msgPass.textContent = "Debes incluir un numero"
+//   }  else if (!passInput.match(symbol)) {
+//     msgPass.textContent = "Debe incluir un simbolo"
+//   } else {
+//     msgPass.innerHTML = "contraseña correcta"
+//     password.style.border = "3px solid green"
+//   };
+// };
+
+
+
+// const init = () => {
+//   form.addEventListener("submit", emailValid);
+//   form.addEventListener("submit", passValid);
+
+// }
+
+// init();
+
+
+// EJ 3
+const form = document.getElementById("toDo");
+const input = document.getElementById("task");
+const lista = document.querySelector(".lista");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const saveLocalStorage = taskList => {
+  localStorage.setItem("tasks", JSON.stringify(taskList))
 };
 
+const creatTask = task => `<li>${task.name}</li>`;
 
-const passValid = e => {
+
+const renderTask = todo => {
+  lista.innerHTML = todo.map(task => creatTask(task)).join("")
+}
+
+
+const addTask = e => {
   e.preventDefault();
 
-  const passInput = password.value.trim();
-  const lowerCase = /[a-z]/g;
-  const upperCase = /[A-Z]/g;
-  const number = /[0-9]/g;
-  const symbol = /[.*+-?^${}()|[]\_@]/g;
+  const taskName = input.value.trim();
 
-  if(passInput == "") {
-    alert("El campo contraseña esta vacio");
+  if(!taskName.length) {
+    alert("Ingresa una tarea");
     return;
+  } else if (tasks.some(task => task.name === taskName)) {
+    alert("Ya existe esa tarea");
+    return;
+  } else {
+    tasks = [... tasks, {name: taskName}];
+    input.value = "";
   }
 
 
-  if(!passInput.match(lowerCase)) {
-    msgPass.textContent = "Debes incluir una minuscula";
-  } else if (!passInput.match(upperCase)) {
-    msgPass.textContent = "Debes incluir una mayuscula"
-  } else if (!passInput.match(number)) {
-    msgPass.textContent = "Debes incluir un numero"
-  }  else if (!passInput.match(symbol)) {
-    msgPass.textContent = "Debe incluir un simbolo"
-  } else {
-    msgPass.innerHTML = "contraseña correcta"
-    password.style.border = "3px solid green"
-  };
-};
-
-
-
-const init = () => {
-  form.addEventListener("submit", emailValid);
-  form.addEventListener("submit", passValid);
-
+  renderTask(tasks);
+  saveLocalStorage(tasks);
 }
 
-init();
+const init = () => {
+  renderTask(tasks);
+  form.addEventListener("submit", addTask);
+}
+
+
+init ();
